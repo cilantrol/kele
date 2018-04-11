6a2499801d06a2df092c310959806f403da8bce1
William Gao <gaowhui@gmail.com>
2018-04-11 21:52:59 +0000

    user info done

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'pp'
+require 'json'
 
   class Kele
     include HTTParty
@@ -18,10 +19,21 @@ require 'pp'
       raise "Error Invalid/Wrong parameters" if response.code == 404 || response['auth_token'].nil?
       @token = response['auth_token']
     end
+    
+    # get user info
+    def get_me
+      response = self.class.get(('/users/me'), headers: {
+        # "authorization" => @token 
+        authorization: @token
+      })
+      # response.body => parse to JSON
+      JSON.parse(response.body)
+    end
+    
   end
   
   # Usage Example
-  party = Kele.new('email@email', 'password')
-  pp party.tokens
-
-
+  # using pp will default in JSON
+  party = Kele.new('asdf', 'asdf')
+  party.tokens
+  p party.get_me
