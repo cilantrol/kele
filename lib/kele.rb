d97c2658ab1f99447d798bb2d0846a06372bebf5
William Gao <gaowhui@gmail.com>
2018-04-12 11:28:08 +0000

    need to verify message

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,10 +2,14 @@ require 'httparty'
 require 'pp'
 require 'json'
 require './lib/roadmap.rb'
+require './lib/secrets.rb'
+
+include Secrets
 
   class Kele
     include HTTParty
     include Roadmap
+
     base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(email, password)
@@ -25,10 +29,8 @@ require './lib/roadmap.rb'
     # get user info
     def get_me
       response = self.class.get(('/users/me'), headers: {
-        # "authorization" => @token 
         authorization: @token
       })
-      # response.body => parse to JSON
       JSON.parse(response.body)
     end
     
@@ -37,16 +39,41 @@ require './lib/roadmap.rb'
       response = self.class.get(("/mentors/#{mentor_id}/student_availability"), headers:{
         authorization: @token
       })
-      # response.body => parse to JSON
       JSON.parse(response.body)
     end
     
+    # get messages
+    def get_messages(message_id = nil)
+      if message_id == nil
+        response = self.class.get(("/message_threads"), headers: {
+          authorization: @token
+        })
+      else
+        response = self.class.get(("/message_threads?page=#{message_id}"), headers:{
+          authorization: @token
+        })
+      end
+      JSON.parse(response.body)
+    end
+    
+    # create messages
+    def create_message(email, receiver_id, token, subject, message)
+      response = self.class.post(("/messages"),
+        values:  {
+                      'sender':         email,
+                      'recipient_id':   receiver_id,
+                      'token':          token,
+                      'subject':        subject,
+                      'stripped-text':  message},
+                      
+        headers: {    authorization:    @token})
+    end
+    
   end
-  
+
   # Usage Example
   # using pp will default in JSON
-  # party = Kele.new('asdf', 'asdf')
+  party = Kele.new($user_email, $user_password)
   party.tokens
-  # p party.get_me
-  # mentor_id = 10
-  # p party.get_mentor_availability(mentor_id)
+  p party.get_messages
+  # p party.create_message('gaowhui@gmail.com', 2, 'token?', 'asdasdfasdf', 'body of message')
