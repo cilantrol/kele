7f5c5a6df72f48bd45e1f3e69ebf0698abe8b643
William Gao <gaowhui@gmail.com>
2018-04-12 18:10:03 +0000

    Kele-0.0.1 done

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -15,4 +15,13 @@ module Roadmap
     JSON.parse(response.body)
   end
   
+  # get remaining checkpoints
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get(("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"), 
+    headers: {
+      authorization: @token
+    })
+    JSON.parse(response.body)
+  end
+  
 end
